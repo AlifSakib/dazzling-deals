48654a7360cd2c0a94fa962cde1b0ee939b4f014
AlifSakib <alifsakib@gmail.com>
2022-10-07 16:55:58 +0600

    delet from cart functionality added

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../utilities/fakedb";
 import Cart from "./Cart";
 import ReviewItem from "./ReviewItem";
 
@@ -8,11 +9,21 @@ const Order = () => {
   const { products, initialCart } = loadedData;
   const [cart, setCart] = useState(initialCart);
 
+  const deletItem = (id) => {
+    const rest = cart.filter((item) => item.id !== id);
+    setCart(rest);
+    removeFromDb(id);
+  };
+
   return (
     <div className="flex  mx-auto lg:w-6/12 md:w-5/6 my-24">
       <div className="w-1/2">
         {cart.map((item) => (
-          <ReviewItem key={item.id} item={item}></ReviewItem>
+          <ReviewItem
+            key={item.id}
+            item={item}
+            deletItem={deletItem}
+          ></ReviewItem>
         ))}
       </div>
       <div className="w-1/2 font-montserrat bg-sky-400 h-96 py-10 rounded-lg mx-8 sticky top-0">
--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -2,8 +2,8 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const ReviewItem = ({ item }) => {
-  const { name, price, quantity, img } = item;
+const ReviewItem = ({ item, deletItem }) => {
+  const { id, name, price, quantity, img } = item;
   return (
     <div className="flex w-full font-montserrat border border-slate-300 rounded-lg p-2 mb-3">
       <div>
@@ -16,7 +16,10 @@ const ReviewItem = ({ item }) => {
           <p>Quantity : {quantity}</p>
         </div>
         <div>
-          <button className="bg-red-200 px-3 py-2 rounded-full text-red-600">
+          <button
+            onClick={() => deletItem(id)}
+            className="bg-red-200 px-3 py-2 rounded-full text-red-600"
+          >
             <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
           </button>
         </div>
